0b0944d3db53d0a72b0752bf8f9122dd5d2cb3ea
Marcio <marcio@kazale.com>
2017-08-18 18:39:30 +0100

    Controller cadastro PJ

---
+++ src/main/java/com/kazale/pontointeligente/api/controllers/CadastroPJController.java
@@ -0,0 +1,145 @@
+package com.kazale.pontointeligente.api.controllers;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kazale.pontointeligente.api.dtos.CadastroPJDto;
+import com.kazale.pontointeligente.api.entities.Empresa;
+import com.kazale.pontointeligente.api.entities.Funcionario;
+import com.kazale.pontointeligente.api.enums.PerfilEnum;
+import com.kazale.pontointeligente.api.response.Response;
+import com.kazale.pontointeligente.api.services.EmpresaService;
+import com.kazale.pontointeligente.api.services.FuncionarioService;
+import com.kazale.pontointeligente.api.utils.PasswordUtils;
+
+@RestController
+@RequestMapping("/api/cadastrar-pj")
+@CrossOrigin(origins = "*")
+public class CadastroPJController {
+
+	private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Autowired
+	private EmpresaService empresaService;
+
+	public CadastroPJController() {
+	}
+
+	/**
+	 * Cadastra uma pessoa jurídica no sistema.
+	 * 
+	 * @param cadastroPJDto
+	 * @param result
+	 * @return ResponseEntity<Response<CadastroPJDto>>
+	 * @throws NoSuchAlgorithmException
+	 */
+	@PostMapping
+	public ResponseEntity<Response<CadastroPJDto>> cadastrar(@Valid @RequestBody CadastroPJDto cadastroPJDto,
+			BindingResult result) throws NoSuchAlgorithmException {
+		log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
+		Response<CadastroPJDto> response = new Response<CadastroPJDto>();
+
+		validarDadosExistentes(cadastroPJDto, result);
+		Empresa empresa = this.converterDtoParaEmpresa(cadastroPJDto);
+		Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPJDto, result);
+
+		if (result.hasErrors()) {
+			log.error("Erro validando dados de cadastro PJ: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		this.empresaService.persistir(empresa);
+		funcionario.setEmpresa(empresa);
+		this.funcionarioService.persistir(funcionario);
+
+		response.setData(this.converterCadastroPJDto(funcionario));
+		return ResponseEntity.ok(response);
+	}
+
+	/**
+	 * Verifica se a empresa ou funcionário já existem na base de dados.
+	 * 
+	 * @param cadastroPJDto
+	 * @param result
+	 */
+	private void validarDadosExistentes(CadastroPJDto cadastroPJDto, BindingResult result) {
+		this.empresaService.buscarPorCnpj(cadastroPJDto.getCnpj())
+				.ifPresent(emp -> result.addError(new ObjectError("empresa", "Empresa já existente.")));
+
+		this.funcionarioService.buscarPorCpf(cadastroPJDto.getCpf())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "CPF já existente.")));
+
+		this.funcionarioService.buscarPorEmail(cadastroPJDto.getEmail())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "Email já existente.")));
+	}
+
+	/**
+	 * Converte os dados do DTO para empresa.
+	 * 
+	 * @param cadastroPJDto
+	 * @return Empresa
+	 */
+	private Empresa converterDtoParaEmpresa(CadastroPJDto cadastroPJDto) {
+		Empresa empresa = new Empresa();
+		empresa.setCnpj(cadastroPJDto.getCnpj());
+		empresa.setRazaoSocial(cadastroPJDto.getRazaoSocial());
+
+		return empresa;
+	}
+
+	/**
+	 * Converte os dados do DTO para funcionário.
+	 * 
+	 * @param cadastroPJDto
+	 * @param result
+	 * @return Funcionario
+	 * @throws NoSuchAlgorithmException
+	 */
+	private Funcionario converterDtoParaFuncionario(CadastroPJDto cadastroPJDto, BindingResult result)
+			throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(cadastroPJDto.getNome());
+		funcionario.setEmail(cadastroPJDto.getEmail());
+		funcionario.setCpf(cadastroPJDto.getCpf());
+		funcionario.setPerfil(PerfilEnum.ROLE_ADMIN);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt(cadastroPJDto.getSenha()));
+
+		return funcionario;
+	}
+
+	/**
+	 * Popula o DTO de cadastro com os dados do funcionário e empresa.
+	 * 
+	 * @param funcionario
+	 * @return CadastroPJDto
+	 */
+	private CadastroPJDto converterCadastroPJDto(Funcionario funcionario) {
+		CadastroPJDto cadastroPJDto = new CadastroPJDto();
+		cadastroPJDto.setId(funcionario.getId());
+		cadastroPJDto.setNome(funcionario.getNome());
+		cadastroPJDto.setEmail(funcionario.getEmail());
+		cadastroPJDto.setCpf(funcionario.getCpf());
+		cadastroPJDto.setRazaoSocial(funcionario.getEmpresa().getRazaoSocial());
+		cadastroPJDto.setCnpj(funcionario.getEmpresa().getCnpj());
+
+		return cadastroPJDto;
+	}
+
+}
---
+++ src/main/java/com/kazale/pontointeligente/api/dtos/CadastroPJDto.java
@@ -0,0 +1,96 @@
+package com.kazale.pontointeligente.api.dtos;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+public class CadastroPJDto {
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String cpf;
+	private String razaoSocial;
+	private String cnpj;
+
+	public CadastroPJDto() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@NotEmpty(message = "Nome não pode ser vazio.")
+	@Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracteres.")
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@NotEmpty(message = "Email não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+	@Email(message="Email inválido.")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@NotEmpty(message = "Senha não pode ser vazia.")
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@NotEmpty(message = "CPF não pode ser vazio.")
+	@CPF(message="CPF inválido")
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@NotEmpty(message = "Razão social não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Razão social deve conter entre 5 e 200 caracteres.")
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@NotEmpty(message = "CNPJ não pode ser vazio.")
+	@CNPJ(message="CNPJ inválido.")
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public String toString() {
+		return "CadastroPJDto [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + "]";
+	}
+
+}
